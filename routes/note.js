ffc9c98509c0e6eb4ce261eb747e6abe22147913
Olusanya <olusanyajolaoluwa@gmail.com>
2023-08-31 23:07:18 +0100

    updates

--- routes/note.js
+++ routes/note.js
@@ -7,6 +7,12 @@ router
   .post(noteController.createNewNote)
   .get(noteController.getAllNotes);
 
+router
+  .route("/:id")
+  .get(noteController.getNote)
+  .put(noteController.updateNote)
+  .delete(noteController.deleteNote);
+
 router.route("/:userId").get(noteController.getAllNotesByUser);
 
 module.exports = router;
